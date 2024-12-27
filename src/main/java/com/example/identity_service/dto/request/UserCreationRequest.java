974d6a74ef3fddaedece291a111c6595eea091d6
Noahvnp <norahpvn@gmail.com>
2024-12-27 21:56:48 +0700

    feat: using Lombok and MapStruct

--- src/main/java/com/example/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/example/identity_service/dto/request/UserCreationRequest.java
@@ -3,61 +3,28 @@ package com.example.identity_service.dto.request;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
     @Size(min = 3, message = "INVALID_USERNAME")
-    private String username;
+    String username;
 
     @Size(min = 4, message = "INVALID_PASSWORD")
-    private String password;
+    String password;
 
-    @NotEmpty(message =  "Firstname must be not empty.")
-    private String firstname;
+    @NotNull(message = "EMPTY_FIRSTNAME")
+    String firstname;
 
-    @NotEmpty(message =  "Lastname must be not empty.")
-    private String lastname;
+    @NotNull(message = "EMPTY_LASTNAME")
+    String lastname;
 
-    private LocalDate dob;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
+    LocalDate dob;
 }
