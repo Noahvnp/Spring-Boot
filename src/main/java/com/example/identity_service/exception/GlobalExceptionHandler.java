a5a91f1a9983382a66599e685dcd7478aa336103
Noahvnp <norahpvn@gmail.com>
2025-01-02 09:51:40 +0700

    refactor: log unhandler exception

--- src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
@@ -1,16 +1,19 @@
 package com.example.identity_service.exception;
 
 import com.example.identity_service.dto.request.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(@NotNull RuntimeException exception) {
+        log.error("Exception: ", exception);
         ApiResponse apiResponse = new ApiResponse();
 
         apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
